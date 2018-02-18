27f965f8b7023a909ff1bd4abb64fd15b68336c3
Michel Gawron <michel.gawron@devinci.fr>
2018-02-18 16:35:08 +0100

    feature(michelin scraping): Adding some scraping features issue(michelin scraping): timeout issue with requests file(notes.txt): adding a notes file

--- scripts/michelin.js
+++ scripts/michelin.js
@@ -3,53 +3,100 @@
  * This module is mainly used to get restaurants from hte michelin website
  */
 var exports = module.exports = {};
-var phantom = require('phantom');
-var Xray = require('x-ray');
-var x = Xray();
+let phantom = require('phantom');
+let Xray = require('x-ray');
+let x = Xray({
+    filters: {
+        removeSpace:
+            /**
+             * Function used in order to remove useless spaces we get when scraping the website
+             * @param value
+             * @returns {string}
+             */
+            function (value) {
+                // Replacing \n characters
+                value = value.replace("\n", "");
 
+                // Removing head spaces
+                while (value.charAt(0) === " ") {
+                    value = value.substring(1);
+                }
 
-var getRestaurantsXray = function (url) {
-    x(url, '.poi-search-result@html')(function (err, content) {
-        x(content, ['.ds-1col@attr-gtm-title'])(function(err, contentbis){
-            console.log(contentbis);
-        })
-    });
+                // Reversing the string
+                value = value.split("").reverse().join("");
+
+                // Removing tail spaces
+                while (value.charAt(0) === " ") {
+                    value = value.substring(1);
+                }
+                return value.split("").reverse().join("");
+            }
+    }
+});
+
+/**
+ * Creating the request for our scraping according to the parameters given by the user
+ * @param city
+ * @param foodType
+ * @param starred
+ * @returns {string}
+ */
+let createURLQuery = function (city, foodType, starred) {
+    return 'https://restaurant.michelin.fr/restaurants/' + city + "/"
+        + foodType + "/"
+        + ((starred) ? "restaurants_michelin" : "");
 };
 
-var getRestaurantsPhantom = function (url) {
-    // Creating phantom variables to store our instances and pages
-    var sitepage = null;
-    var phInstance = null;
+/**
+ * This method goes on the michelin website and scrapes all restaurants
+ * Let's get the restaurants name, url on michelin, stars,
+ * @param url
+ */
+let getRestaurantsXray = function (url) {
+    getNumberPages(url, function (numPages) {
+        var tabInfos = [];
+        for (let i = 1; i <= numPages; i++) {
+            let urlScrape = url + "/page-" + i;
 
-    console.log("Got into function");
+            // First pass to get the results section
+            x(urlScrape, '.poi-search-result', ['.ds-1col@html'])(function (err, content) {
+                // Second pass on each element in order to get only relevant information
+                console.log(urlScrape);
+                try {
+                    // TODO: Make less requests because the server seems to not be able to handle it
+                    for (let restaurant of content) {
+                        // Getting title, food type, price and michelin url
+                        x(restaurant, {
+                            title: '.poi_card-display-title | removeSpace',
+                            foodType: '.poi_card-display-cuisines | removeSpace',
+                            price: '.poi_card-display-price | removeSpace',
+                            michelinUrl: '.poi-card-link@href'
+                        })(function (err, attributes) {
+                            tabInfos.push(attributes);
+                        })
+                    }
+                }
+                catch(error){
+                    console.log(content)
+                    console.log(err)
+                }
+            });
+        }
 
-    phantom.create()
-    // Creating phantom instance and page
-        .then(instance => {
-            phInstance = instance;
-            return instance.createPage();
-        })
-        // Loading an url in our instance
-        .then(page => {
-            sitepage = page;
-            console.log(url);
-            return sitepage.open(url);
-        })
-        // Awaiting for the page to load and getting status
-        .then(status => {
-            console.log("Status: " + status);
-            return sitepage.property('content');
-        })
-        // Getting page's content
-        .then(content => {
-            console.log("Content: " + content);
-        })
-        // Catching errors
-        .catch(error => {
-            console.log(error);
-            phInstance.exit();
-        });
+    })
 };
 
-exports.getRestaurants = getRestaurantsPhantom;
+/**
+ * Function used to get the number of pages on a michelin webpage
+ * @param url
+ * @param callback
+ */
+let getNumberPages = function (url, callback) {
+    // We get the list of pages, and get the number before the last one
+    x(url, '.item-list-first', ['.mr-pager-item'])(function (err, content) {
+        callback(content[content.length - 2]);
+    })
+};
+
 exports.getRestaurantsXray = getRestaurantsXray;
+exports.createURLQuery = createURLQuery;
--- server.js
+++ server.js
@@ -2,13 +2,16 @@
  * Created by M2469215 on 16/02/2018.
  */
 
-var express = require('express');
-var michelin = require("./scripts/michelin.js");
+let express = require('express');
+let michelin = require("./scripts/michelin.js");
 
 michelin.getRestaurantsXray("https://restaurant.michelin.fr/restaurants/france");
 
-var app = express();
+let app = express();
 
+/**
+ * Basic test function
+ */
 app.use('/', function(req, res, next){
     console.log("A connection has been recorded");
 });
