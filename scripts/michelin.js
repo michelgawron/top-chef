bfcd88f775a5ded2f2d4763866bd56ede2fad0cd
Gawron Michel <michel.gawron@saint-gobain.com>
2018-02-24 15:42:50 +0100

    Adding new features to michelin scraping feature(michelin scrpaing): Testing a new way to make requests

--- scripts/michelin.js
+++ scripts/michelin.js
@@ -7,32 +7,31 @@ let phantom = require('phantom');
 let Xray = require('x-ray');
 let x = Xray({
     filters: {
-        removeSpace:
-            /**
-             * Function used in order to remove useless spaces we get when scraping the website
-             * @param value
-             * @returns {string}
-             */
+        removeSpace: /**
+         * Function used in order to remove useless spaces we get when scraping the website
+         * @param value
+         * @returns {string}
+         */
             function (value) {
-                // Replacing \n characters
-                value = value.replace("\n", "");
+            // Replacing \n characters
+            value = value.replace("\n", "");
 
-                // Removing head spaces
-                while (value.charAt(0) === " ") {
-                    value = value.substring(1);
-                }
-
-                // Reversing the string
-                value = value.split("").reverse().join("");
-
-                // Removing tail spaces
-                while (value.charAt(0) === " ") {
-                    value = value.substring(1);
-                }
-                return value.split("").reverse().join("");
+            // Removing head spaces
+            while (value.charAt(0) === " ") {
+                value = value.substring(1);
             }
+
+            // Reversing the string
+            value = value.split("").reverse().join("");
+
+            // Removing tail spaces
+            while (value.charAt(0) === " ") {
+                value = value.substring(1);
+            }
+            return value.split("").reverse().join("");
+        }
     }
-});
+}).timeout(0);
 
 /**
  * Creating the request for our scraping according to the parameters given by the user
@@ -55,34 +54,36 @@ let createURLQuery = function (city, foodType, starred) {
 let getRestaurantsXray = function (url) {
     getNumberPages(url, function (numPages) {
         var tabInfos = [];
-        for (let i = 1; i <= numPages; i++) {
-            let urlScrape = url + "/page-" + i;
+        var tabUrls = [];
 
-            // First pass to get the results section
-            x(urlScrape, '.poi-search-result', ['.ds-1col@html'])(function (err, content) {
-                // Second pass on each element in order to get only relevant information
-                console.log(urlScrape);
-                try {
-                    // TODO: Make less requests because the server seems to not be able to handle it
-                    for (let restaurant of content) {
-                        // Getting title, food type, price and michelin url
-                        x(restaurant, {
-                            title: '.poi_card-display-title | removeSpace',
-                            foodType: '.poi_card-display-cuisines | removeSpace',
-                            price: '.poi_card-display-price | removeSpace',
-                            michelinUrl: '.poi-card-link@href'
-                        })(function (err, attributes) {
-                            tabInfos.push(attributes);
-                        })
-                    }
+        // First pass to get the results section
+        x(url, '.poi-search-result', ['.ds-1col@html'])(function (err, content) {
+            // Second pass on each element in order to get only relevant information
+            console.log(url);
+            try {
+                for (let restaurant of content) {
+                    // Getting title, food type, price and michelin url
+                    x(restaurant, {
+                        title: '.poi_card-display-title | removeSpace',
+                        foodType: '.poi_card-display-cuisines | removeSpace',
+                        price: '.poi_card-display-price | removeSpace',
+                        michelinUrl: '.poi-card-link@href'
+                    })(function (err, attributes) {
+                        tabInfos.push(attributes);
+                    });
                 }
-                catch(error){
-                    console.log(content)
-                    console.log(err)
-                }
-            });
-        }
-
+                console.log(tabInfos.length);
+            }
+            catch (error) {
+                console.log(content);
+                console.log(err);
+            }
+        }).paginate('.mr-pager-item.last a@href').limit(10).then(
+            function(res){
+                console.log(res.length)
+            }
+        );
+        console.log(tabInfos.length);
     })
 };
 
